68fc19b6ea8f177246142fd10ea8495712e548e9
Inweyder <kacsul115@gmail.com>
2023-02-28 13:05:51 +0100

    dodanie podstawy uzupełnania spisu

--- index.php
+++ index.php
@@ -6,6 +6,19 @@
 </head>
 <body>
 
+<form action="index.php" method="get">
+    <input type="date" name="data">
+    <label for="data">Data</label>
+    <input type="Time" name="czas_w">
+    <label for="czas_w">Czas Wejścia</label>
+    <input type="Time" name="czas_o">
+    <label for="czas_o">Czas Opuszczenia</label>
+    <select name="pracownik">
+    <option>Pierwszy</option>
+    <option>Drugi</option>
+    <option>Trzeci</option>
+    <option>Czwarty</option>
+    </select>
 </body>
 <?php
 
