d539fe7438d2bc192fe7c096090b06631e646c09
Inweyder <kacsul115@gmail.com>
2023-02-28 13:34:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,19 +6,29 @@
 </head>
 <body>
 
-<form action="index.php" method="get">
-    <input type="date" name="data">
+<form action="index.php" method="post">
+    
     <label for="data">Data</label>
-    <input type="Time" name="czas_w">
-    <label for="czas_w">Czas Wejścia</label>
-    <input type="Time" name="czas_o">
-    <label for="czas_o">Czas Opuszczenia</label>
-    <select name="pracownik">
-    <option>Pierwszy</option>
-    <option>Drugi</option>
-    <option>Trzeci</option>
-    <option>Czwarty</option>
+    <input type="date" id="date" name="data" />
+
+    <label for="godz_wejscia">Czas Wejścia</label>
+    <input type="Time" id="godz_wejscia" name="godz_wejscia" />
+
+    <label for="godz_opuszczenia">Czas Opuszczenia</label>
+    <input type="Time" id="" name="godz_opuszczenia" />
+
+    <label for="id_pracownika">ID Pracownika</label>
+    <input type="number"id="id_pracownika" name="id_pracownika"/>
+
+
+    <label for="dzial">Dział</label>
+    <option Value="Sprzedaż">Sprzedaż</option>
+    <option Value="IT">IT</option>
+    <option Value="Produkcja">Produkcja</option>
+    <option Value="Serwis">Serwis</option>
     </select>
+
+    <input type="submit" value="Submit" />
 </body>
 <?php
 
