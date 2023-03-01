a330b0530ba48c6d95f9dcf5db2ff1f73284b3cb
Inweyder <kacsul115@gmail.com>
2023-03-01 12:39:47 +0100

    WYSWIETANIE Z BAZY
    Do poprawienia

--- index.php
+++ index.php
@@ -8,16 +8,16 @@
 <div class=div>
   <form action="index.php" method="post">
     <label for="date">Data</label>
-    <input type="date" id="date" name="date" />
+    <input type="date" id="date" name="date" /><br>
 
     <label for="godz_wejscia">Godzina Wejscia</label>
-    <input type="time" id="godz_wejscia" name="godz_wejscia" />
+    <input type="time" id="godz_wejscia" name="godz_wejscia" /><br>
 
     <label for="godz_opuszczenia">Godzina Opuszczenia</label>
-    <input type="time" id="godz_opuszczenia" name="godz_opuszczenia" />
+    <input type="time" id="godz_opuszczenia" name="godz_opuszczenia" /><br>
 
     <label for="id_pracownika">ID Pracownika</label>
-    <input type="number" id="id_pracownika" name="id_pracownika" />
+    <input type="number" id="id_pracownika" name="id_pracownika" /><br>
 
     <label for="dzial">Dział</label>
     <select id="dzial" name="dzial">
@@ -29,6 +29,10 @@
     </select>
   
     <input type="submit" value="Submit" />
+
+    <form action="index.php" method="GET">
+            <input type="submit" name="wyswietl" value="Wyświetl">
+
   </form>
 </div>
 
@@ -54,7 +58,7 @@ $dzial = $_POST['dzial'];
 
 $sql = "INSERT INTO spis (data, godz_wejscia, godz_opuszczenia, id_pracownika, dzial)
 VALUES ('$date', '$godz_wejscia', '$godz_opuszczenia', $id_pracownika, '$dzial')";
-if ($_POST){
+if (null != $_POST['date']  ){
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
@@ -62,6 +66,32 @@ if ($conn->query($sql) === TRUE) {
 }
 }
 $conn->close();
+
+
+if (isset($_GET['action'])) {
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+$sql1 = "SELECT * FROM spis ORDER BY id DESC LIMIT 7";
+$result = $conn->query($sql1);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['data'] . "</td>";
+        echo "<td>" . $row['godz_wejscia'] . "</td>";
+        echo "<td>" . $row['godz_opuszczenia'] . "</td>";
+        echo "<td>" . $row['id_pracownika'] . "</td>";
+        echo "<td>" . $row['dzial'] . "</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "Brak wyników";
+}
+$conn->close();
+}
 ?> 
 
 </html>
